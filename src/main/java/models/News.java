a1594cc2ac2eb8884906e9455ffa88d08c7efdca
FrankKamala <frankkamala@gmail.com>
2020-01-20 00:13:29 +0300

    add news var and constructor

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,14 @@
 package models;
 
 public class News {
+
+    private int id;
+    private String content;
+    private int departmentId;
+
+    public News(String content, int departmentId) {
+        this.content = content;
+        this.departmentId = departmentId;
+    }
+
 }
