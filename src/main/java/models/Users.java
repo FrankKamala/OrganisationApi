1cb2bc268768c7798d5a5ffc4bcdbf655753a803
FrankKamala <frankkamala@gmail.com>
2020-01-20 00:14:17 +0300

    add user at and constructor

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,15 @@
 package models;
 
 public class Users {
+    private String name;
+    private String position;
+    private String roles;
+    private int id;
+
+    public Users(String roles, String position,String name) {
+        this.name = name;
+        this.roles = roles;
+        this.position = position;
+    }
+
 }
