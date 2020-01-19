be88a092a5d42c43dbdfe91bc17478a288b4c7fb
FrankKamala <frankkamala@gmail.com>
2020-01-20 00:12:50 +0300

    add deptmnt var and constructor

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,16 @@
 package models;
 
 public class Department {
+
+    private String name;
+    private String description;
+    private int noOfEmployees;
+    private int id;
+
+    public Department(String name, String description, int noOfEmployees) {
+        this.name = name;
+        this.description = description;
+        this.noOfEmployees = noOfEmployees;
+    }
+
 }
