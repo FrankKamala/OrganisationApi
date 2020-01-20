dbcae75ddb6be8a563775e317711d225be55ccc3
FrankKamala <frankkamala@gmail.com>
2020-01-20 12:21:06 +0300

    from h2 to Postgress

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,32 +1,36 @@
-SET MODE PostgreSQL;
+
+CREATE DATABASE organisation;
+
+\c organisation;
 
 
-
-CREATE TABLE IF NOT EXISTS department(
+CREATE TABLE department(
     id SERIAL PRIMARY KEY,
     name VARCHAR,
     description VARCHAR,
-    noOfEmployees int
+    noOfEmployees INTEGER
 );
 
-CREATE TABLE  IF NOT EXISTS news(
+CREATE TABLE news(
     id SERIAL PRIMARY KEY ,
     content VARCHAR,
     departmentId INTEGER
 );
 
-CREATE TABLE  IF NOT EXISTS  users(
+CREATE TABLE users(
     id SERIAL PRIMARY KEY ,
     name VARCHAR,
     position VARCHAR,
     roles VARCHAR
 );
 
-CREATE TABLE  IF NOT EXISTS departments_users(
+CREATE TABLE departments_users(
     id SERIAL PRIMARY KEY ,
     usersId INTEGER,
     departmentId INTEGER
 );
+
+CREATE DATABASE organisation_test WITH TEMPLATE organisation;
 
 
 
